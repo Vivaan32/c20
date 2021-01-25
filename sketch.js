9ea44902095bbf9ad07be68ec8b70954318b2815
Vivaan32 <76661668+Vivaan32@users.noreply.github.com>
2021-01-24 19:48:49 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+    var fixedRect, movingRect;
+    
+    
+    
+    function setup() {
+      createCanvas(800,400);
+     fixedRect= createSprite(500,200,50,80);
+     fixedRect.shapeColor= "red";
+
+     movingRect= createSprite(700,300,80,30);
+     movingRect.shapeColor= "blue";
+
+     movingRect.debug= true;
+     fixedRect.debug= true;
+
+    }
+
+    function draw() {
+      background(0); 
+      
+      movingRect.x= mouseX;
+      movingRect.y= mouseY;
+
+      console.log(movingRect.x-fixedRect.x);
+
+      if(movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2 &&
+          fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2 && movingRect.y-fixedRect.y<movingRect.height/2
+          +fixedRect.height/2 && fixedRect.y-movingRect.y<movingRect.height/2+fixedRect.height/2
+        ){
+        movingRect.shapeColor="green";
+      }
+      else{
+        movingRect.shapeColor= "blue";
+      }
+      
+
+      drawSprites();
+    }
